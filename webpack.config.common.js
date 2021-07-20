a480d19c5654775f2de23f4ef3ebca3e2f03ffdf
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-07-20 12:21:43 +0100

    setup webpack configs

---
+++ webpack.config.common.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+    },
+    module: {
+        rules: [{
+            test: /\.html$/,
+            use: ["html-loader"],
+        }, ],
+    },
+};
