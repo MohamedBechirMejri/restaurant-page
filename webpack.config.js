bec9bbba626b1baff7530d006d889aca28ea516a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-07-20 14:40:12 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    mode: "production",
     output: {
-        filename: "main.js",
+        filename: "[name].js",
+
         path: path.resolve(__dirname, "dist"),
+    },
+/*    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+            }),
+        ],
+    },*/
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
     },
 };
