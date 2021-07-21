2b1995e70ff7e68c09a9388e00e27ce7a6363141
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-07-21 23:29:09 +0100

    generate homepage on pageload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { homepage } from "./js/homepage";
-import { menu } from "./js/menu";
-import { contactUs } from "./js/contact-us";
+import {
+    generateHomepage
+} from "./js/homepage";
+import {
+    menu
+} from "./js/menu";
+import {
+    contactUs
+} from "./js/contact-us";
 
 import "./css/style.css";
 
+generateHomepage();
